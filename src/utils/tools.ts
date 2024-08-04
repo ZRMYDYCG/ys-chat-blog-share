d8bc9dbca196c0ead99fda4e95658fbf9e8087e9
ZRMYDYCG <547471919@qq.com>
2024-08-04 12:21:31 +0800

    chore: src/utils __debounce防抖函数添加函数文档

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -2,12 +2,19 @@
  * @Author: ZRMYDYCG 547471919@qq.com
  * @Date: 2024-08-03 23:30:06
  * @LastEditors: ZRMYDYCG 547471919@qq.com
- * @LastEditTime: 2024-08-04 12:10:17
+ * @LastEditTime: 2024-08-04 12:16:46
  * @FilePath: \blog-front-vue3\src\utils\tools.ts
  * @Description: 一些工具的封装
  */
 import type { IImagePos } from "@/types/utils";
 
+
+/**
+ * 防抖函数
+ * @param fn 需要防抖处理的函数
+ * @param delay 延迟时间，单位为毫秒
+ * @returns 返回一个新的函数，该函数会在指定延迟时间后执行传入的函数
+ */
 export function __debounce(fn: Function, delay: number) {
   let timer: number | null = null;
   return function (this: unknown, ...args: any[]) {
